9efd0a042d6a65035836794a783662d5e2e73e94
JORDAN HENRIQUE MOREIRA ZANONI <52969396+zanonijordan@users.noreply.github.com>
2022-09-05 23:11:33 -0300

    Update db.js

--- inc/db.js
+++ inc/db.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   database: 'saboroso',
-  password: 'italiazanoni07',
+  password: '',
   multipleStatements: true
 });
 
